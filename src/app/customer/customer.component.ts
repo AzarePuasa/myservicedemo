29b0632fe58bf1cdd1cbef4f64cff5df193187a1
AzarePuasa <azarepuasa@azarepuasa.com>
2018-09-06 16:28:39 +0800

    first commit

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer.service';
+import { Customer } from '../customer';
+
+import { Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+
+  customers: Customer[];
+  
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit() {
+    this.customerService.getCustomers()
+    .subscribe(data => this.customers = data);
+  }
+
+}
